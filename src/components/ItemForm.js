dde155eca9f32934be53958de68bf01c9b4436d6
Mwagiru <joymwagiru@gmail.com>
2022-05-27 06:11:18 +0300

    Completed Assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,50 @@
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+function ItemForm({onItemFormSubmit}){
+  const [formData , setFormData]= useState({
+    name:"",
+    category: "Produce",
+  });
+  function handleChange(event){
+    setFormData({
+      ...formData, [event.target.name]: event.target.value,
+    });
+  }
+  function handleSubmit(event){
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      ...formData
+    })
+  }
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
-      <button type="submit">Add to List</button>
+    <form className="NewItem" onSubmit={handleSubmit}>
+    <label>
+    Name:
+    <input
+    type="text"
+    name="name"
+    value={formData.name}
+    onChange={handleChange}
+    />
+    </label>
+    <label>
+    Category:
+    <select
+    name="category"
+    value={formData.category}
+    onChange={handleChange}
+    >
+     <option value="Produce">Produce</option>
+     <option value="Dairy">Dairy</option>
+     <option value="Dessert">Dessert</option>
+    </select>
+    </label>
+    <button type="submit">Add</button>
     </form>
-  );
+  )
 }
-
 export default ItemForm;
